5e27fbc4d644365060844ec881111c2e499b1435
Yusuf26862 <yusuf26862@gmail.com>
2023-07-13 09:43:12 -0700

    Don't like the idea of puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	//string initialisation
+    char Mystr[] = "\"Programming is like building a multilingual puzzle";
+    
+    puts(Mystr); //writing the string to stdout
+    
+    return 0;
+}
