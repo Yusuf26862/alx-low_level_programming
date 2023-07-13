4e9b4b4c0094150cc0d0a942400f7d0f65fb90c3
Yusuf26862 <yusuf26862@gmail.com>
2023-07-13 10:53:18 -0700

    fixed p4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 int main(void)
 {
 /* Assign thei character */
-char Mystr[] = "\"Programming is like building a multilingual puzzle";
-puts(Mystr); /* writing the string to stdout */
-return (0);
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	puts(Mystr); /* writing the string to stdout */
+	return (0);
 }
