4e3fee5a016476d4e4497e3fcc3dd6e8c19d7a28
Yusuf26862 <yusuf26862@gmail.com>
2023-07-14 19:14:14 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alp;
+	char calp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		putchar(alp);
+	}
+	for (calp = 'A'; calp <= 'Z'; calp++)
+        {
+                putchar(calp);
+        }
+	putchar('\n');
+	return (0);
+}
