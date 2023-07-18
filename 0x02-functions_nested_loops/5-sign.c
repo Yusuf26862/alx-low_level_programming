d9c7797054685141fd9c9a6ac593f3d320e46d7f
Yusuf26862 <yusuf26862@gmail.com>
2023-07-18 11:31:09 -0700

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * print_sign - check the chcek the sign of a number.
+ *
+ * @n: The character to be checked.
+ *
+ * Return: 1 if n > 0 , 0 otherwise if <= o.
+ */
+int print_sign(int n);
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (0);
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
