7f68b3ea6f20aaff2bbb02bbcdca50e4815c7ebf
Yusuf26862 <yusuf26862@gmail.com>
2023-07-18 13:28:41 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit - prints the last digite.
  *
- * @c: The character to be checked.
+ * @n: The character to be checked.
  *
  * Return: 1 if character is lowercase, 0 otherwise.
  */
@@ -13,5 +13,9 @@ int print_last_digit(int n)
 	int ld;
 
 	ld = n % 10;
+	if (ld < 0)
+	{
+		ld *= -1;
+	}
 	return (ld);
 }
