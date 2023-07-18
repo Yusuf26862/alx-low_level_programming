a0a2776ad455f4ea1b246b62e1f2a47eaf2ecc50
Yusuf26862 <yusuf26862@gmail.com>
2023-07-18 11:57:45 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,9 @@
  *
  * @n: The character to be checked.
  *
- * Return: 1 if n > 0 , 0 otherwise if <= o.
+ * Return: 1 if n > 0
+ * 	-1 if <= 0
+ *	0 if == 0
  */
 int print_sign(int n);
 int print_sign(int n)
@@ -15,14 +17,14 @@ int print_sign(int n)
 		putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
+	{
+		putchar('-');
+		return (0);
+	}
+	else
 	{
 		putchar('-');
 		return (-1);
-	}
-	else if (n == 0)
-	{
-		putchar('0');
-		return (0);
 	}
 }
