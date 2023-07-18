d4ae99df9fc688be5af7450f553608b79074a87b
Yusuf26862 <yusuf26862@gmail.com>
2023-07-18 12:24:48 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _abs - print the absolute value of number
+ *
+ * @c: The character to be checked.
+ *
+ * Return: 1 if character is lowercase, 0 otherwise.
+ */
+int _abs(int c);
+int _abs(int c)
+{
+	abs(c)
+	return (0);
+}
