f8c3c222b0002e50397661f84a76df72f2cea224
Yusuf26862 <yusuf26862@gmail.com>
2023-07-21 08:53:18 -0700

    9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * fizzbuzz - print numbers from 1 - 100 and check for fizz, buzz or fizzbuzz
+ *
+ * return - always 0
+ *
+ */
+
+void fizzbuzz(void);
+void fizzbuzz(void)
+{
+	int n;
+
+	n = 1;
+	while (n < 100)
+	{
+		if ((n % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((n % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((n % 3) == 0 && (n % 5) == 0);
+		{
+			printf("FizzBuzz ");
+		}
+	//	else
+	//	{
+		printf("%d ", n);
+	//	}
+		n++;
+	}
+	printf("\n");
+}
