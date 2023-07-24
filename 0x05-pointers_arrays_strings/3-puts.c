ed3c4947e4d819e13367a254991dd07709cb46d2
Yusuf26862 <yusuf26862@gmail.com>
2023-07-24 11:05:27 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _puts - check the length of the string.
+ *
+ * @str: The varable of string to print to stdout.
+ *
+ */
+
+void _puts(char *str);
+void _puts(char *str)
+{
+	while (*str)
+	{
+		_putchar(*str);
+		*str++;
+	}
+	_putchar('\n');
+}
