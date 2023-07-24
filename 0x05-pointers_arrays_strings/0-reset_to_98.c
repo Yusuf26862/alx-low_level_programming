b1169f689ac17d4d41b382dd274b4f678148c474
Yusuf26862 <yusuf26862@gmail.com>
2023-07-24 07:02:48 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - reset varable to 98
+ *
+ * @n: The varable of character to be reset to.
+ *
+ * Return: Always 0
+ */
+
+void reset_to_98(int *n);
+void reset_to_98(int *n)
+{
+	*n = 98
+	return (0);
+}
