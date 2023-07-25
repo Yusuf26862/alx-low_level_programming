37debb1ed271be6f41e3e582db22311ecebcb52f
Yusuf26862 <yusuf26862@gmail.com>
2023-07-24 17:49:22 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,12 @@ void puts2(char *str)
 	int index = 0, len = 0;
 
 	while (str[index++])
+	{
 		len++;
-
+	}
 	for (index = 0; index < len; index += 2)
+	{
 		putchar(str[index]);
-
-	_putchar('\n');
+	}
+	putchar('\n');
 }
