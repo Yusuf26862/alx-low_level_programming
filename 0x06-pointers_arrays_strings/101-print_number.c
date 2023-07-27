ab142f4fb26806edf599878dfedae739381ae356
Yusuf26862 <yusuf26862@gmail.com>
2023-07-27 16:18:41 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  * print_number - Prints an integer.
@@ -15,7 +16,8 @@ void print_number(int n)
 	}
 
 	if ((num / 10) > 0)
+	{
 		print_number(num / 10);
-
+	}
 	_putchar((num % 10) + '0');
 }
