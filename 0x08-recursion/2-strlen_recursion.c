0c9e8e568b4ca49e711729483bd932e1eb39e907
Yusuf26862 <yusuf26862@gmail.com>
2023-08-02 09:29:37 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,8 +10,13 @@
 int _strlen_recursion(char *s);
 int _strlen_recursion(char *s)
 {
+	int lenght;
+
+	length = 0;
 	if (s[0] != '\0')
 	{
-		return (1 + _strlen_recursion(s + 1));
+		_strlen_recursion(s + 1);
+		length++
 	}
+	return (length);
 }
